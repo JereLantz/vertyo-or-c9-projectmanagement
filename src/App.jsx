12afc3996a942b3fb11f8959bcb19049ba846125
Jere Lantz <lantz.jere@gmail.com>
2025-03-22 16:22:18 +0200

    switching from no project selected to the create new project form implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import {useState} from "react"
+
 import NewProject from "./components/NewProject";
 import NoProjSel from "./components/NoProjSel";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    })
+
+    function handleStartAddProject(){
+        setProjectsState(prev =>{
+            return {
+                ...prev,
+                selectedProjectId:null,
+            }
+        })
+    }
+
+    let content
+    if(projectsState.selectedProjectId === null){
+        content = <NewProject />
+    }
+    else if(projectsState.selectedProjectId === undefined){
+        content = <NoProjSel onStartAddProject={handleStartAddProject}/>
+    }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjSel />
+      <Sidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
