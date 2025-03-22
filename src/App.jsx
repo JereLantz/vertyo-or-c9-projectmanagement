25aeb878aebdeb33745f455fe8739253fef99d3d
Jere Lantz <lantz.jere@gmail.com>
2025-03-22 13:49:04 +0200

    created the view that is displayed when no project is selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjSel from "./components/NoProjSel";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjSel />
     </main>
   );
 }
