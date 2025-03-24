936c88929b7d34bfb3833e717da870b77c922cea
Jere Lantz <lantz.jere@gmail.com>
2025-03-24 11:24:19 +0200

    implemented displaying saved project

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import {useState} from "react"
 import NewProject from "./components/NewProject";
 import NoProjSel from "./components/NoProjSel";
 import Sidebar from "./components/Sidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
         projects: []
     })
+
+    function handleSelectProject(id){
+        setProjectsState(prev =>{
+            return {
+                ...prev,
+                selectedProjectId:id,
+            }
+        })
+    }
 
     function handleStartAddProject(){
         setProjectsState(prev =>{
@@ -43,7 +53,9 @@ function App() {
         })
     }
 
-    let content
+    const selectedProject = projectsState.projects.find(project =>project.id ===projectsState.selectedProjectId)
+    let content = <SelectedProject project={selectedProject}/>
+
     if(projectsState.selectedProjectId === null){
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
     }
@@ -53,7 +65,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject}/>
+      <Sidebar onSelectProject={handleSelectProject} projects={projectsState.projects} onStartAddProject={handleStartAddProject}/>
       {content}
     </main>
   );
